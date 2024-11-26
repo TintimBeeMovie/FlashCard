bceaee5cac69c3edb11d34680bb234e86a6121e5
TintimBeeMovie <valentimcostar@gmail.com>
2024-11-26 14:33:39 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Engenharia Aeroespacial',
+    'O que é a aerodinâmica?',
+    'Aerodinâmica é o estudo do comportamento dos gases, especialmente o ar, em movimento e como ele interage com os corpos em voo, como aviões e foguetes.'
+)
+
+criaCartao(
+    'Engenharia Aeroespacial',
+    'O que é a propulsão em aeronaves',
+    'Propulsão é o sistema responsável por gerar a força necessária para mover uma aeronave, geralmente utilizado motores a jato ou hélicies.'
+)
+
+criaCartao(
+    'Engenharia Aeroespacial',
+    'O que é o controle de voo?',
+    'O controle de voo é o sistema que permite que o piloto ou o sistema automático gerencie as condições de voo, ajustando os comandos para manter a estabilidade e a trajetória da aeronave.'
+)
+
+criaCartao(
+    'Física Engenharia Aeroespacial',
+    'Uma aeronave está voando a um velocidade de 200m/s a uma altitude onde a densidade do ar é 1,2kg/m³. Qual a força de sustentação necessária para manter a aeronave em voo reto e nivelado, se a área da asa da aeronave é 30m²?',
+    'L = 864000. A força de sustentação necessária é 864.000 N (Newtons).'
+)
